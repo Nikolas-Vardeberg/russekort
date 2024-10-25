68de771da4fd3aa8137e381b431901b1be901281
niklusrook <nikolas.vardeberg1@gmail.com>
2024-10-25 13:02:41 +0200

    article schema init

---
+++ sanity/schemaTypes/pages/article.ts
@@ -0,0 +1,86 @@
+import { defineType } from "sanity";
+
+
+
+export const article = defineType({
+    type: "document",
+    name: "article",
+    title: "Article",
+    groups: [
+        {
+            name: "general",
+            title: "General",
+            default: true,
+        },
+        {
+            name: "byline",
+            title: "Byline",
+        },
+        {
+            name: "seo",
+            title: "SEO",
+        },
+    ],
+    fields: [
+        {
+            name: "title",
+            type: "string",
+            title: "Title",
+            group: "general",
+        },
+        {
+            name: "mainImage",
+            type: "image",
+            title: "Main image",
+            description: "The main image of the article",
+            options: {
+                hotspot: true,
+            },
+            validation: (Rule) => Rule.required(),
+            group: "general",
+        },
+        {
+            name: "slug",
+            type: "slug",
+            options: {
+                source: 'title',
+            },
+            group: "general",
+        },
+        {
+            name: "publishedAt",
+            type: "datetime",
+            title: "Pulisert",
+            description: "Dato for publisering av artikkelen",
+            group: "byline",
+            initialValue: new Date().toISOString(),
+            validation: (Rule) => Rule.required(),
+        },
+        //TODO: BLOCKS
+        //TODO: EDITOR
+        //TODO: BLOCKS
+        //TODO: SEO
+    ],
+    orderings: [
+        {
+            title: "Publiseringsdato (nyeste først)",
+            name: "publishDateDesc",
+            by: [{ field: "publishedAt", direction: 'desc'}],
+        },
+        {
+            title: "Publiseringsdato (eldste først)",
+            name: "publishDateAsc",
+            by: [{ field: "publishedAt", direction: "asc"}],
+        },
+        {
+            title: "Tittel (A-Å)",
+            name: "titleAsc",
+            by: [{ field: "title", direction: "asc"}],
+        },
+        {
+            title: "Tittel (A-Å)",
+            name: "titleDesc",
+            by: [{ field: 'title', direction: "asc"}],
+        }
+    ]
+})
